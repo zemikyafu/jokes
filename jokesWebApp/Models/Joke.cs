6c8cb79a81ec4b051863a6d8c97153fcc1d9a629
zemikyafu <zemikfcb@gmail.com>
2022-09-26 14:07:40 +0300

    Add project files.

---
+++ jokesWebApp/Models/Joke.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jokesWebApp.Models
+{
+    public class Joke
+    {
+
+        /* prob*/
+        public int Id { get; set; }
+        public String  JokeQuestions { get; set; }
+
+        public String  JokeAnswers { get; set; }
+
+        /* ctor*/
+        public Joke()
+        {
+
+        }
+    }
+}
